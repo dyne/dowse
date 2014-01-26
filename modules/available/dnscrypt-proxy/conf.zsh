8270e338af5e8f605610e795dd4d109729c5936e
Jaromil <jaromil@dyne.org>
2014-01-26 19:05:17 +0100

    setup/start/stop functions for modules

--- modules/available/dnscrypt-proxy/conf.zsh
+++ modules/available/dnscrypt-proxy/conf.zsh
@@ -4,9 +4,17 @@ command -v dnscrypt-proxy > /dev/null
 
 { test $? = 0 } || { print "dnscrypt-proxy not found."; return 1 }
 
-cat <<EOF >> $DOWSE/run/dnsmasq.conf
+module_setup() {
+    cat <<EOF >> $DOWSE/run/dnsmasq.conf
 server=127.0.0.1#5353
 no-resolv
 EOF
+}
 
-dnscrypt-proxy -a 127.0.0.1:5353 -d
+module_start() {
+    dnscrypt-proxy -a 127.0.0.1:5353 -d
+}
+
+module_stop() {
+    killall dnscrypt-proxy
+}
