b47eae30adfd4395271d26b172156e2ac075c423
Jaromil <jaromil@dyne.org>
2014-01-27 17:00:35 +0100

    dnscrypt defaults to no-log eu servers and adds key authentication

--- modules/available/dnscrypt-proxy/conf.zsh
+++ modules/available/dnscrypt-proxy/conf.zsh
@@ -9,7 +9,13 @@ EOF
 }
 
 module_start() {
-    setuidgid $dowseuid dnscrypt-proxy -a 127.0.0.1:5353 -d -p $DOWSE/run/dnscrypt-proxy.pid
+	# DNSCrypt.EU (Netherlands)
+    setuidgid $dowseuid dnscrypt-proxy -a 127.0.0.1:5353 -d -p $DOWSE/run/dnscrypt-proxy.pid \
+	-r 176.56.237.171:443 -k 67C0:0F2C:21C5:5481:45DD:7CB4:6A27:1AF2:EB96:9931:40A3:09B6:2B8D:1653:1185:9C66 -N 2.dnscrypt-cert.resolver1.dnscrypt.eu 
+
+        # OpenDNS
+	# -r 208.67.220.220:443 -k B735:1140:206F:225D:3E2B:D822:D7FD:691E:A1C3:3CC8:D666:8D0C:BE04:BFAB:CA43:FB79 -N 2.dnscrypt-cert.opendns.com
+
 }
 
 module_stop() {
