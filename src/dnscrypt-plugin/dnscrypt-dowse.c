d19c58828be329ec1982d314caf45b2f2b63c5b3
Jaromil <jaromil@dyne.org>
2016-09-07 21:47:30 +0200

    dnscrypt plugin now connects to redis and formats domainlist

--- src/dnscrypt-plugin/dnscrypt-dowse.c
+++ src/dnscrypt-plugin/dnscrypt-dowse.c
@@ -2,28 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <dirent.h>
+
 #include <dnscrypt/plugin.h>
 #include <ldns/ldns.h>
 
+#include <hiredis/hiredis.h>
+#include <jemalloc/jemalloc.h>
+
+#include <redis.h>
+#include <database.h>
+#include <epoch.h>
+
+#include "hashmap.h"
+
 DCPLUGIN_MAIN(__FILE__);
 
-const char *
-dcplugin_description(DCPlugin * const dcplugin)
-{
+#define MAX_QUERY 512
+#define MAX_DOMAIN 256
+#define MAX_TLD 32
+
+typedef struct {
+	char query[MAX_QUERY]; // incoming query
+	char domain[MAX_DOMAIN]; // domain part parsed (2nd last dot)
+	char tld[MAX_TLD]; // tld (domain extension, 1st dot)
+
+	// hash map of known domains
+	map_t domainlist;
+
+	redisContext *redis;
+	redisReply   *reply;
+}  plugin_data_t;
+
+size_t trim(char *out, size_t len, const char *str);
+void load_domainlist(const char *path, plugin_data_t *data);
+int free_domainlist_f(any_t arg, any_t element);
+char *extract_domain(char *address, plugin_data_t *data);
+
+const char * dcplugin_description(DCPlugin * const dcplugin) {
 	return "Dowse plugin to filter dnscrypt queries";
 }
 
 
-const char *
-dcplugin_long_description(DCPlugin * const dcplugin)
-{
+const char * dcplugin_long_description(DCPlugin * const dcplugin) {
 	return
 		"This plugin checks all settings in Dowse and operates filtering on dnscrypt queries accordingly\n";
 }
 
-int
-dcplugin_init(DCPlugin * const dcplugin, int argc, char *argv[])
-{
+int dcplugin_init(DCPlugin * const dcplugin, int argc, char *argv[]) {
+
+	char *listpath = getenv("DOWSE_DOMAINLIST");
+	if(!listpath) {
+		fprintf(stderr,"error: environmental variable DOWSE_DOMAINLIST not set\n");
+		return 1;
+	}
+
+	plugin_data_t *data = malloc(sizeof(plugin_data_t));
+
+	if(listpath) {
+		fprintf(stderr, "Loading domainlist from: %s\n", listpath);
+		load_domainlist(listpath, data);
+	}
+
+	data->redis = connect_redis(REDIS_HOST, REDIS_PORT, db_dynamic);
+	// TODO: check succesful result
+
+	dcplugin_set_user_data(dcplugin, data);
+    
 	return 0;
 }
 
@@ -31,7 +76,14 @@ dcplugin_init(DCPlugin * const dcplugin, int argc, char *argv[])
 int
 dcplugin_destroy(DCPlugin * const dcplugin)
 {
-	void *data = dcplugin_get_user_data(dcplugin);
+
+	plugin_data_t *data = dcplugin_get_user_data(dcplugin);
+
+	hashmap_iterate(data->domainlist, free_domainlist_f, NULL);
+	hashmap_free(data->domainlist);
+		
+	redisFree(data->redis);
+
 	return 0;
 }
 
@@ -46,9 +98,215 @@ DCPluginSyncFilterResult dcplugin_sync_post_filter(DCPlugin *dcplugin, DCPluginD
 		return DCP_SYNC_FILTER_RESULT_ERROR;
 	}
 
+
+
+
+
+	// test print out domain from packet
+	{
+		ldns_rr  *question;
+		char     *resolved;
+		char     *extracted;
+		long long int val;
+		char *sval;
+		int res;
+
+		char outnew[MAX_OUTPUT];
+
+		time_t epoch_t;
+
+		question = ldns_rr_list_rr(ldns_pkt_question(packet), 0U);
+		if ((resolved = ldns_rdf2str(ldns_rr_owner(question))) == NULL)
+			return DCP_SYNC_FILTER_RESULT_FATAL;
+
+		plugin_data_t *data = dcplugin_get_user_data(dcplugin);
+		extracted = extract_domain(resolved, data);
+		data->reply = redisCommand(data->redis, "INCR dns_query_%s", extracted);
+		val = data->reply->integer;
+		freeReplyObject(data->reply);
+		data->reply = redisCommand(data->redis, "EXPIRE dns_query_%s %u", extracted, 21600); // DNS_HIT_EXPIRE
+
+		time(&epoch_t);
+
+
+		// compose the path of the detected query
+		snprintf(outnew,MAX_OUTPUT,
+		         "DNS,%s,%lld,%lu,%s,%s",
+		         "from",val,
+		         epoch_t,extracted,data->tld);
+
+		res = hashmap_get(data->domainlist, extracted, (void**)(&sval));
+		if(res==MAP_OK) {// add domain group
+			strncat(outnew,",",2);
+			strncat(outnew,sval,MAX_OUTPUT-128);
+			//snprintf(outnew,MAX_OUTPUT,"%s,%s",outnew,sval);
+		}
+
+		data->reply = redisCommand(data->redis, "PUBLISH dns-query-channel %s", outnew);
+		freeReplyObject(data->reply);
+
+
+		fprintf(stderr,"DNS: %s\n", outnew);
+		
+	}
+
+
+#if 0
+	// test print out ip from packet
+	{
+		ldns_rr_list *answers;
+		ldns_rr      *answer;
+		char         *answer_str;
+		ldns_rr_type  type;
+		size_t        answers_count;
+		size_t        i;
+
+		answers = ldns_pkt_answer(packet);
+		answers_count = ldns_rr_list_rr_count(answers);
+		for (i = (size_t) 0U; i < answers_count; i++) {
+			answer = ldns_rr_list_rr(answers, i);
+			type = ldns_rr_get_type(answer);
+			if (type != LDNS_RR_TYPE_A && type != LDNS_RR_TYPE_AAAA) {
+				continue;
+			}
+			if ((answer_str = ldns_rdf2str(ldns_rr_a_address(answer))) == NULL) {
+				return DCP_SYNC_FILTER_RESULT_FATAL;
+			}
+			fprintf(stderr,"ip: %s\n", answer_str);
+			free(answer_str);
+		}
+	}
+#endif
+
 	ldns_pkt_free(packet);
 
 	return DCP_SYNC_FILTER_RESULT_OK;
 
 
+}
+
+
+
+// Stores the trimmed input string into the given output buffer, which must be
+// large enough to store the result.  If it is too small, the output is
+// truncated.
+size_t trim(char *out, size_t len, const char *str) {
+	if(len == 0)
+		return 0;
+
+	const char *end;
+	size_t out_size;
+
+	// Trim leading space
+	while(isspace(*str)) str++;
+
+	if(*str == 0)  // All spaces?
+	{
+		*out = 0;
+		return 1;
+	}
+
+	// Trim trailing space
+	end = str + strlen(str) - 1;
+	while(end > str && isspace(*end)) end--;
+	end++;
+
+	// Set output size to minimum of trimmed string length and buffer size minus 1
+	out_size = (end - str) < len-1 ? (end - str) : len-1;
+
+	// Copy trimmed string and add null terminator
+	memcpy(out, str, out_size);
+	out[out_size] = 0;
+
+	return out_size;
+}
+#define MAX_LINE 512
+
+
+void load_domainlist(const char *path, plugin_data_t *data) {
+    char line[MAX_LINE];
+    char trimmed[MAX_LINE];
+    DIR *listdir = 0;
+    struct dirent *dp;
+    FILE *fp;
+
+    data->domainlist = hashmap_new();
+
+    // parse all files in directory
+    listdir = opendir(path);
+    if(!listdir) {
+        perror(path);
+        exit(1); }
+
+    // read file by file
+    dp = readdir (listdir);
+    while (dp) {
+        char fullpath[MAX_LINE];
+        snprintf(fullpath,MAX_LINE,"%s/%s",path,dp->d_name);
+        // open and read line by line
+        fp = fopen(fullpath,"r");
+        if(!fp) {
+            perror(fullpath);
+            continue; }
+        while(fgets(line,MAX_LINE, fp)) {
+            // save lines in hashmap with filename as value
+            if(line[0]=='#') continue; // skip comments
+            trim(trimmed, strlen(line), line);
+            if(trimmed[0]=='\0') continue; // skip blank lines
+            // logerr("(%u) %s\t%s", trimmed[0], trimmed, dp->d_name);
+            hashmap_put(data->domainlist, strdup(trimmed), strdup(dp->d_name));
+        }
+        fclose(fp);
+        dp = readdir (listdir);
+    }
+    closedir(listdir);
+    fprintf(stderr,"size of parsed domain-list: %u\n", hashmap_length(data->domainlist));
+}
+
+
+int free_domainlist_f(any_t arg, any_t element) {
+	free(element);
+	return MAP_OK;
+}
+
+
+char *extract_domain(char *address, plugin_data_t *data) {
+    // extracts the last two or three strings of a dotted domain string
+
+    int c;
+    int dots = 0;
+    int first = 1;
+    char *last;
+    int positions = 2; // minimum, can become three if sld too short
+    int len = strlen(address);
+
+    /* logerr("extract_domain: %s (%u)",address, len); */
+
+    if(len<3) return(NULL); // a.i
+
+    if(len>MAX_QUERY) {
+        fprintf(stderr,"extract_domain: string too long (%s - %u)", address, len);
+        return(NULL); }
+
+    data->domain[len+1]='\0';
+    for(c=len; c>=0; c--) {
+        last=address+c;
+        if(*last=='.') {
+            dots++;
+            // take the tld as first dot hits
+            if(first) {
+                strncpy(data->tld,last,MAX_TLD);
+                first=0; }
+        }
+        if(dots>=positions) {
+            char *test = strtok(last+1,".");
+            if( strlen(test) > 3 ) break; // its not a short SLD
+            else positions++;
+        }
+        data->domain[c]=*last;
+    }
+
+    // logerr("extracted: %s (%p) (dots: %u)", domain+c+1, domain+c+1, dots);
+
+    return(data->domain+c+1);
 }
