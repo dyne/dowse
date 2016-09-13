98647756ca1bd744456c9653d9278742bf0bf614
Jaromil <jaromil@dyne.org>
2016-09-13 09:16:30 +0200

    misc build fixes

--- src/compile.sh
+++ src/compile.sh
@@ -16,9 +16,9 @@ LDFLAGS="-fPIC -fPIE -pie"
 
 
 [[ -x $R/build/bin/$1 ]] && {
-	act "$1 found in $R/build/bin/$1"
-	act "delete it from build/bin to force recompilation"
-	return 0 }
+    act "$1 found in $R/build/bin/$1"
+    act "delete it from build/bin to force recompilation"
+    return 0 }
 
 notice "Compiling $1"
 
@@ -26,7 +26,7 @@ case $1 in
     seccrond)
         pushd $R/src/seccrond
         CFLAGS="$CFLAGS" make
-		install -s -p seccrond $R/build/bin
+        install -s -p seccrond $R/build/bin
         popd
         ;;
 
@@ -37,7 +37,7 @@ case $1 in
         popd
         ;;
 
-	# first kore, then webui (which is built with kore)
+    # first kore, then webui (which is built with kore)
     kore)
         [[ -x $R/build/kore ]] || {
             pushd $R/src/kore
@@ -47,44 +47,40 @@ case $1 in
         ;;
     webui)
         pushd $R/src/webui
-		notice "Generating WebUI configuration"
-		act "chroot: $HOME/.dowse"
-		act "uid:    $USER"
-		cat <<EOF > conf/webui.conf
+        notice "Generating WebUI configuration"
+        act "chroot: $HOME/.dowse"
+        act "uid:    $USER"
+        cat <<EOF > conf/webui.conf
 chroot    $HOME/.dowse
 runas     $USER
 EOF
-		cat conf/webui.conf.dist >> conf/webui.conf
+        cat conf/webui.conf.dist >> conf/webui.conf
         $R/src/kore/kore build
-		install -s -p webui $R/build/bin
+        install -s -p webui $R/build/bin
         popd
         ;;
 
     netdata)
-        [[ -x $R/build/netdata ]] || {
-            pushd $R/src/netdata
-            ./autogen.sh
-            CFLAGS="$CFLAGS" \
-                  ./configure --prefix=${PREFIX}/netdata \
-                  --datarootdir=${PREFIX}/netdata \
-                  --with-webdir=${PREFIX}/netdata \
-                  --localstatedir=$HOME/.dowse \
-                  --sysconfdir=/etc/dowse &&
-                make &&
-                install -s -p src/netdata $R/build/bin
-            popd
+        pushd $R/src/netdata
+        ./autogen.sh
+        CFLAGS="$CFLAGS" \
+              ./configure --prefix=${PREFIX}/netdata \
+              --datarootdir=${PREFIX}/netdata \
+              --with-webdir=${PREFIX}/netdata \
+              --localstatedir=$HOME/.dowse \
+              --sysconfdir=/etc/dowse &&
+            make &&
+            install -s -p src/netdata $R/build/bin
+        popd
 
-        }
         ;;
     netdiscover)
-        [[ -x $R/build/netdiscover ]] || {
-            pushd $R/src/netdiscover
-			autoreconf && \
+        pushd $R/src/netdiscover
+        autoreconf && \
             CFLAGS="$CFLAGS" ./configure --prefix=${PREFIX} && \
-                make && \
-                install -s -p src/netdiscover $R/build/bin
-            popd
-        }
+            make && \
+            install -s -p src/netdiscover $R/build/bin
+        popd
         ;;
 
     sup)
@@ -107,17 +103,17 @@ EOF
         popd
         ;;
 
-	dnscrypt_dowse.so)
-		pushd $R/src/dnscrypt-plugin
-		./configure && make && \
-			install -s -p .libs/dnscrypt_dowse.so $R/build/bin
-		popd
-		;;
+    dnscrypt_dowse.so)
+        pushd $R/src/dnscrypt-plugin
+        ./configure && make && \
+            install -s -p .libs/dnscrypt_dowse.so $R/build/bin
+        popd
+        ;;
 
     pgld)
         pushd $R/src/pgl
         ./configure --without-qt4 --disable-dbus --enable-lowmem \
-					--disable-networkmanager \
+                    --disable-networkmanager \
                     --prefix ${PREFIX}/pgl \
                     --sysconfdir ${HOME}/.dowse/pgl/etc \
                     --with-initddir=${PREFIX}/pgl/init.d \
