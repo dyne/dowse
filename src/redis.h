8e8699b33dd82a22bb4914deb4256c696203e3b5
Jaromil <jaromil@dyne.org>
2016-10-02 16:01:34 +0200

    small fixes, mostly to the build
    shared redis object made pure header

--- src/redis.h
+++ src/redis.h
@@ -1,3 +1,7 @@
+
+#ifndef __REDIS_H__
+#define __REDIS_H__
+
 #include <hiredis/hiredis.h>
 
 #define REDIS_HOST "127.0.0.1"
@@ -14,4 +18,28 @@
 /*     struct redisReply **element; // elements vector for REDIS_REPLY_ARRAY */
 /* } redisReply; */
 
-redisContext *connect_redis(char *host, int port, int db);
+redisContext *connect_redis(char *host, int port, int db) {
+	redisContext   *rx;
+	redisReply     *reply;
+	fprintf(stderr,"Connecting to redis on %s port %u\n", host, port);
+	struct timeval timeout = { 1, 500000 };
+	rx = redisConnectWithTimeout(host, port, timeout);
+	/* rx = redisConnect(REDIS_HOST, REDIS_PORT); */
+
+	if (rx == NULL || rx->err) {
+		if (rx) {
+			fprintf(stderr,"Connection error: %s\n", rx->errstr);
+			redisFree(rx);
+		} else {
+			fprintf(stderr,"Connection error: can't allocate redis context\n");
+		}
+	}
+	// select the dynamic database where is dns_query_channel
+	reply = redisCommand(rx, "SELECT %u", db);
+	// TODO: check if result is OK
+	// fprintf(stderr,"SELECT: %s\n", reply->str);
+	freeReplyObject(reply);
+	return rx;
+}
+
+#endif
