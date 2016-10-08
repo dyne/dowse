6a3fb7d99b5ef6c851ad457c12ea36a115218f4e
Jaromil <jaromil@dyne.org>
2016-10-08 12:58:57 +0200

    webui: new generic asset server
    also added websocket.html example

---
+++ src/webui/src/assetsrv.c
@@ -0,0 +1,58 @@
+#include <kore.h>
+#include <http.h>
+
+#include <execinfo.h>
+
+#include "log.h"
+#include "assetmap.h"
+
+int free_assetmap_f(any_t arg, any_t element) {
+	kore_free(element);
+	return MAP_OK;
+}
+void free_assetmap(map_t amap) {
+	if(!amap) return;
+	hashmap_iterate(amap, free_assetmap_f, NULL);
+	hashmap_free(amap);
+}
+
+int serve_asset(struct http_request *req) {
+	map_t assetmap = NULL;
+	char *asset_name;
+	asset_t *rasset;
+
+	// void **back;
+	// int backlen;
+	
+	func("CALL: serve_asset");
+	// write(2 , "CALL: serve_asset\n", 18);
+	// backtrace(back, backlen);
+	// backtrace_symbols_fd(back, backlen, 0);
+
+	http_populate_get(req);
+
+	if( http_argument_get_string(req, "o", &asset_name) ) {
+		func("asset file requested: %s", asset_name);
+
+		assetmap = asset_init();
+		func("assetmap initialised");
+
+		if( hashmap_get(assetmap, asset_name, (void**)&rasset) == MAP_MISSING) {
+			free_assetmap(assetmap);
+			func("asset not found (404)");
+			http_response(req, 404, NULL, 0);
+			return(KORE_RESULT_OK);
+		}
+
+		http_response_header(req, "content-type", rasset->type);
+		http_response(req, 200, rasset->data, rasset->len);
+		
+		free_assetmap(assetmap);
+		return(KORE_RESULT_OK);
+	}
+
+	http_response_header(req, "content-type", "text/html");
+	http_response(req, 200, asset_welcome_html, asset_len_welcome_html);
+	return(KORE_RESULT_OK);
+
+}
