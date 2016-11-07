b55964b229581aa357a9efa83700027b6602b8c9
Jaromil <jaromil@dyne.org>
2016-11-07 15:07:37 +0100

    more error log functions

--- src/webui/src/log.c
+++ src/webui/src/log.c
@@ -8,17 +8,36 @@ void func(const char *fmt, ...) {
 #if (DEBUG==1)
 	va_list args;
 
-	char msg[512];
+	char msg[256];
 	size_t len;
 
 	va_start(args, fmt);
 
 	vsnprintf(msg, 512, fmt, args);
 	len = strlen(msg);
+	write(2, "[D] ", 4);
 	write(2, msg, len);
 	write(2, "\n", 1);
 	fsync(2);
 
 	va_end(args);
 #endif
+}
+
+void error(const char *fmt, ...) {
+	va_list args;
+
+	char msg[256];
+	size_t len;
+
+	va_start(args, fmt);
+
+	vsnprintf(msg, 512, fmt, args);
+	len = strlen(msg);
+	write(2, "[!] ", 4);
+	write(2, msg, len);
+	write(2, "\n", 1);
+	fsync(2);
+
+	va_end(args);
 }
