16481d861cd59d29b8867bb19cf54e86c7d3c189
Jaromil <jaromil@dyne.org>
2016-11-07 20:07:15 +0100

    reverted to kmod's log.h

--- src/log.h
+++ src/log.h
@@ -1,58 +1,37 @@
-#ifndef __LOG_H__
-#define __LOG_H__
+/*
+ * kmod - log infrastructure
+ *
+ * Copyright (C) 2012-2013  ProFUSION embedded systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <syslog.h>
 
-// to quickly return 404
-#define HTTP404 err("HTTP 404, %s:%u, %s()",	  \
-                      __FILE__, __LINE__, __func__);	  \
-	http_response(req, 404, NULL, 0); \
-	return(KORE_RESULT_OK)
+/* #include "kmod.h" */
 
-// for use in debugging
-#define FLAG func("reached: %s:%u, %s()", __FILE__, __LINE__, __func__)
+void log_open(bool use_syslog);
+void log_close(void);
+void log_printf(int prio, const char *fmt, ...) _printf_format_(2, 3);
+#define CRIT(...) log_printf(LOG_CRIT, __VA_ARGS__)
+#define ERR(...) log_printf(LOG_ERR, __VA_ARGS__)
+#define WRN(...) log_printf(LOG_WARNING, __VA_ARGS__)
+#define INF(...) log_printf(LOG_INFO, __VA_ARGS__)
+#define DBG(...) log_printf(LOG_DEBUG, __VA_ARGS__)
 
-
-void func(const char *fmt, ...) {
-#if (DEBUG==1)
-	va_list args;
-
-	char msg[256];
-	size_t len;
-
-	va_start(args, fmt);
-
-	vsnprintf(msg, 512, fmt, args);
-	len = strlen(msg);
-	write(2, "[D] ", 4);
-	write(2, msg, len);
-	write(2, "\n", 1);
-	fsync(2);
-
-	va_end(args);
-#endif
-}
-
-void err(const char *fmt, ...) {
-	va_list args;
-
-	char msg[256];
-	size_t len;
-
-	va_start(args, fmt);
-
-	vsnprintf(msg, 512, fmt, args);
-	len = strlen(msg);
-	write(2, "[!] ", 4);
-	write(2, msg, len);
-	write(2, "\n", 1);
-	fsync(2);
-
-	va_end(args);
-}
-
-
-#endif
+struct kmod_ctx;
+void log_setup_kmod_log(struct kmod_ctx *ctx, int priority);
