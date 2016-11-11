44b27dddab14af27c7bc4e8034c0927ec33b1f0a
Jaromil <jaromil@dyne.org>
2016-11-11 01:00:47 +0100

    colors in the logs

--- src/libdowse/log.c
+++ src/libdowse/log.c
@@ -7,6 +7,14 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 void func(const char *fmt, ...) {
 #if (DEBUG==1)
 	va_list args;
@@ -18,7 +26,7 @@ void func(const char *fmt, ...) {
 
 	vsnprintf(msg, 512, fmt, args);
 	len = strlen(msg);
-	write(2, "[D] ", 4);
+	write(2, ANSI_COLOR_BLUE " [D] " ANSI_COLOR_RESET, 5+5+4);
 	write(2, msg, len);
 	write(2, "\n", 1);
 	fsync(2);
@@ -38,7 +46,26 @@ void err(const char *fmt, ...) {
 
 	vsnprintf(msg, 512, fmt, args);
 	len = strlen(msg);
-	write(2, "[!] ", 4);
+	write(2, ANSI_COLOR_RED " [!] " ANSI_COLOR_RESET, 5+5+4);
+	write(2, msg, len);
+	write(2, "\n", 1);
+	fsync(2);
+
+	va_end(args);
+}
+
+
+void notice(const char *fmt, ...) {
+	va_list args;
+
+	char msg[256];
+	size_t len;
+
+	va_start(args, fmt);
+
+	vsnprintf(msg, 512, fmt, args);
+	len = strlen(msg);
+	write(2, ANSI_COLOR_GREEN " (*) " ANSI_COLOR_RESET, 5+5+4);
 	write(2, msg, len);
 	write(2, "\n", 1);
 	fsync(2);
@@ -57,7 +84,7 @@ void act(const char *fmt, ...) {
 
 	vsnprintf(msg, 512, fmt, args);
 	len = strlen(msg);
-	write(2, " .  ", 4);
+	write(2, "  .  ", 5);
 	write(2, msg, len);
 	write(2, "\n", 1);
 	fsync(2);
