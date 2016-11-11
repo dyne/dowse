1e47515ae7776a05042780202611e77f80ca373e
Nicola <nicola@dyne.org>
2016-11-11 14:07:42 +0100

    Added HTML rendering of handling SQL error

--- src/webui/src/webui_debug.h
+++ src/webui/src/webui_debug.h
@@ -1,12 +1,15 @@
 #include <kore.h>
+#include <http.h>
+#include <attributes_set.h>
 
-#ifndef __WHERE_I_AM_
-#define __WHERE_I_AM_
-static char __buf_where_i_am__[256];
+#ifndef _WEBUI_DEBUG_H
+#define _WEBUI_DEBUG_H
+#ifndef __where_i_am__
+static uint8_t __buf_where_i_am__[256];
 
 #define __where_i_am__\
 	(\
-		snprintf(__buf_where_i_am__,sizeof(__buf_where_i_am__),"%s>%s:%d",__FILE__,__func__,__LINE__)?\
+		snprintf(__buf_where_i_am__,sizeof(__buf_where_i_am__),"%s:%d:1 '%s'",__FILE__,__LINE__,__func__)?\
 				__buf_where_i_am__:__buf_where_i_am__)
 	/* TODO Stack trace format in tmp directory ? */
 
@@ -20,4 +23,33 @@ static char __buf_where_i_am__[256];
 #define WEBUI_DEBUG {fprintf(stderr,"WEBUI_DEBUG: %s\n",__where_i_am__);}
 #endif
 
+/* Define mapping with bootstap alert level */
+#define WEBUI_level_error (char*)"danger"
+#define WEBUI_level_warning (char*)"warning"
+#define WEBUI_level_info (char*)"info"
+#define WEBUI_level_success (char*)"success"
+
+/* Definition of function and prototype for add message to TMPL_LOOP */
+void _webui_add_message_level(attributes_set_t *ptr_attrl,const char*level,const char *log_message);
+
+#define WEBUI_DEF_ERROR_LEVEL_MESSAGE_PROTOTYPE(LEVEL) \
+    void webui_add_ ## LEVEL ## _message  (attributes_set_t *ptr_attrl,const char *log_message) ;
+
+WEBUI_DEF_ERROR_LEVEL_MESSAGE_PROTOTYPE(info) ;
+WEBUI_DEF_ERROR_LEVEL_MESSAGE_PROTOTYPE(warning) ;
+WEBUI_DEF_ERROR_LEVEL_MESSAGE_PROTOTYPE(success) ;
+WEBUI_DEF_ERROR_LEVEL_MESSAGE_PROTOTYPE(error);
+
+
+#define WEBUI_DEF_ERROR_LEVEL_MESSAGE(LEVEL) \
+        void webui_add_ ## LEVEL ## _message (attributes_set_t *ptr_attrl,const char *log_message) \
+            { _webui_add_message_level(ptr_attrl, WEBUI_level_ ##LEVEL ,log_message); }
+/**/
+
+#define TMPL_VAR_LEVEL_MESSAGE "level"
+#define TMPL_VAR_TEXT_MESSAGE "text"
+
+#define TMPL_VAR_MESSAGE_LOOP "message_loop"
+
 /* TODO aggiungere stacktrace debug */
+#endif
