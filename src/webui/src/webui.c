e33c034259a61739a8bd80c5ca4c0d50ae0cdfd0
Nicola <nicola@dyne.org>
2016-11-15 19:13:52 +0100

    Clean

--- src/webui/src/webui.c
+++ src/webui/src/webui.c
@@ -18,46 +18,8 @@
  * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
-
-#include <time.h>
-
-#include <kore.h>
-#include <http.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <string.h>
-#include <math.h>
-
-#include <mysql.h>
-
-#include "hashmap.h"
-
-#include <parse-datetime.h>
-#include "template.h"
-#include "webui_debug.h"
-
-#define mb (1024*500)
-#define ml (1024*3)
-
-// used with kore_buf_(create/appendf/free)
-struct kore_buf *buf;
-
-char line[ml];
-char where_condition[ml];
-char *thingsdb;
-MYSQL *db = NULL;
-map_t thing = NULL;
-char *macaddr;
-template_t tmpl;
-
-
-char *thing_get(char *key);
-int sqlquery(char *query,
-		int (*callback)(attributes_set_t *data, int , MYSQL_ROW , MYSQL_FIELD *),
-		attributes_set_t *ptr_attrl);
+#define __WEBUI_MAIN_FILE__
+#include <webui.h>
 
 int relative_time(char *utc, char *out) {
     time_t nowutc;
@@ -147,10 +109,16 @@ int thing_show_cb( attributes_set_t  *data, int argc, MYSQL_ROW argv, MYSQL_FIEL
     //--- In the hashmap data we add to the key "things" the hm element we created.
     (*data)=attr_add(*data,"things",t);
     WEBUI_DEBUG
-    return 0;
+    return 1;
 
 }
 
+
+/* TODO Choose if purge or not.
+ *  What list this callback ?
+ *  How is it made the template?
+ *
+ * */
 int things_list_cb(attributes_set_t *data, int argc, MYSQL_ROW argv, MYSQL_FIELD *azColName)
 {
     int i;
@@ -159,10 +127,13 @@ int things_list_cb(attributes_set_t *data, int argc, MYSQL_ROW argv, MYSQL_FIELD
     attributes_set_t *attr;
      char *button_group_start="<div class=\"btn-group\" role=\"group\" aria-label=\"actions\">";
      char *button_start="<div type=\"button\" class=\"btn btn-default\">";
+     attributes_set_t t;
+     t=attrinit();
+
     WEBUI_DEBUG
 
     for(i=0; i<argc; i++){ // save all fields into the hashmap
-        hashmap_put(thing , azColName[i].name,
+        attrcat(t , azColName[i].name,
                     (argv[i] ? argv[i] : "NULL"));
     }
 
@@ -212,7 +183,6 @@ int thing_show(struct http_request *req) {
   http_populate_get(req);
 
   // we shouldn't free the result in macaddr
-
   if (http_argument_get_string(req, "macaddr", &macaddr)) {
     kore_log(LOG_DEBUG, "thing_show macaddr %s",macaddr);
     //--- prepare where condition
@@ -269,12 +239,13 @@ int things_list(struct http_request *req) {
     char *query = "SELECT * FROM found ORDER BY last DESC";
     template_t tmpl;
     attributes_set_t attributes;
+    map_t thing = NULL;
 
     struct timespec when;
     WEBUI_DEBUG;
     buf = kore_buf_alloc(mb);
 
-    if(!thing) thing = hashmap_new();
+    thing = hashmap_new();
 
     // load template from assets
     template_load
@@ -311,94 +282,12 @@ int things_list(struct http_request *req) {
 char *thing_get(char *key) {
     int res;
     char *sval = NULL;
-    res = hashmap_get(thing, key, (void**)&sval);
-    if(res==MAP_OK)
-        return(sval);
-    else
-        return("NULL");
+    /* TODO Delete ! It's no more needed by ctemplate integration */
+    /*    res = hashmap_get(thing, key, (void**)&sval);
+        if(res==MAP_OK)
+            return(sval);
+        else
+            return("NULL");*/
+    return "NULL";
 }
 
-
-inline void show_error(MYSQL *mysql,attributes_set_t *ptr_attrl) {
-#define __SIZE (2048)
- char *log_message=kore_malloc(__SIZE);
-
- snprintf(log_message, __SIZE,
-                       "Error(%d) [%s] \"%s\"", mysql_errno(mysql),
-                                                mysql_sqlstate(mysql),
-                                                mysql_error(mysql));
- kore_log(LOG_ERR, "%s: [%s]\n", log_message, thingsdb);
-
- webui_add_error_message(ptr_attrl,log_message);
-
-  mysql_close(mysql);
-}
-
-// same as sqlite3_exec
-int sqlquery(char *query,
-		int (*callback)( attributes_set_t*,int , MYSQL_ROW , MYSQL_FIELD *),
-             attributes_set_t *ptr_attrl) {
-  MYSQL_RES *result;
-  MYSQL_ROW values; //  it as an array of char pointers (MYSQL_ROW),
-  MYSQL_FIELD*column;
-  unsigned int num_fields;
-  unsigned int i;
-
-  thingsdb="things";
-  if (thingsdb==NULL) {
-    kore_log(LOG_ERR,
-	     "Error db_things(=$db[things]) environment variable not defined %s>%s:%d",
-	     __FILE__,__func__,__LINE__);
-    return KORE_RESULT_ERROR;
-  }
-  WEBUI_DEBUG;
-  // open db connection
-  if(!db) {
-      db=mysql_init(NULL);
-//     Constant parameted created at compile time
-      if (!mysql_real_connect(db, DB_HOST, DB_USER,DB_PASSWORD,
-                  thingsdb, 0, DB_SOCK_DIRECTORY , 0))  {
-          show_error(db,ptr_attrl);
-          db=NULL;
-          return(KORE_RESULT_ERROR);
-      }
-  }
-  
-
-  WEBUI_DEBUG;
-  // Execute the statement
-  if (mysql_real_query(db, query, strlen(query))) {
-    show_error(db,ptr_attrl);
-    return KORE_RESULT_ERROR;
-  }
-  
-  WEBUI_DEBUG; ;  
-  result=mysql_store_result(db);
-
-  WEBUI_DEBUG; ;
-  num_fields = mysql_num_fields(result);
-  if (num_fields == 0) {
-    kore_log(LOG_ERR,"The query [%s] has returned 0 fields. Is it correct?",query);
-    return KORE_RESULT_ERROR;
-  }
-
-  WEBUI_DEBUG; ;  
-  kore_log(LOG_DEBUG,"The query [%s] has returned [%d] row with [%u] columns.",
-	   query, (int)mysql_affected_rows(db), num_fields);
-  
-  column= mysql_fetch_fields(result);
-  
-  
-  while ((values = mysql_fetch_row(result))!=0) {
-    for (i=0;i<num_fields;i++){
-      kore_log(LOG_DEBUG,"[%d][%s][%s]",i,column[i].name,values[i]);
-    }
-    
-    //--- executing "callback" function pointer that add result in the attributes_set
-    callback(ptr_attrl,(int)num_fields,values,column);
-  }
-  WEBUI_DEBUG; 
-  mysql_free_result(result);
-
-  return 0;
-}
