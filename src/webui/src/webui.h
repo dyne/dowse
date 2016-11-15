fe52a1bf6afba723565892ac39857bd9f4ebe6a1
Nicola <nicola@dyne.org>
2016-11-15 19:17:50 +0100

    Map file into function

---
+++ src/webui/src/webui.h
@@ -0,0 +1,25 @@
+#ifndef __WEBUI_H_
+#define __WEBUI_H_
+#include <webui_debug.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <math.h>
+#include <mysql.h>
+#include <parse-datetime.h>
+#include <hashmap.h>
+#include <time.h>
+#include <kore.h>
+#include <http.h>
+#include <stdio.h>
+
+#include <template.h>
+
+#define mb (1024*500)
+#define ml (1024*3)
+
+#include <webui_common_var.h>
+#include <webui_prototype.h>
+
+#endif
