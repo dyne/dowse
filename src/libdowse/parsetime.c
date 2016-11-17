0ff9415700c93d1eef61065e2650b52350ab2e55
Jaromil <jaromil@dyne.org>
2016-11-17 19:05:57 +0100

    moved relative_time in libdowse

---
+++ src/libdowse/parsetime.c
@@ -0,0 +1,76 @@
+/*  Dowse - embedded WebUI based on Kore.io
+ *
+ *  (c) Copyright 2016 Dyne.org foundation, Amsterdam
+ *  Written by Denis Roio aka jaromil <jaromil@dyne.org>
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#define _XOPEN_SOURCE 500
+#define _POSIX_C_SOURCE 200112L
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+
+int relative_time(char *utc, char *out) {
+    time_t nowutc;
+    time_t now;
+    time_t then;
+    long int deltaSeconds;
+    long int deltaMinutes;
+
+    struct tm tt;
+    // parse utc into a tm struct
+    memset(&tt,0,sizeof(struct tm));
+    if( ! strptime(utc, "%Y-%m-%dT%H:%M:%SZ", &tt) ) {
+        // kore_log(LOG_ERR,"relative_time failed parsing UTC string: %s",utc);
+        if( ! strptime(utc, "%Y-%m-%dT%H:%M:%S", &tt) ) {
+         	sprintf(out,"n/a");
+
+            return 1;
+
+        }
+    	}
+    then = mktime(&tt);
+    
+    // gets what UTC time is now
+    time( &nowutc );
+    now = mktime( gmtime ( &nowutc ) );
+
+    // localtime_r( &rnow, &now );
+    deltaSeconds = now - then;
+    deltaMinutes = (now / 60) - (then / 60);
+
+    if(deltaSeconds < 60)
+        snprintf(out,256,"%ld seconds ago", deltaSeconds);
+    else if (deltaMinutes < 60)
+        snprintf(out,256,"%ld minutes ago", deltaMinutes);
+    else if (deltaMinutes < (24 * 60))
+        snprintf(out,256,"%d hours ago",(int)floor(deltaMinutes/60));
+    else if (deltaMinutes < (24 * 60 * 7))
+        snprintf(out,256,"%d days ago",(int)floor(deltaMinutes/(60 * 24)));
+    else if (deltaMinutes < (24 * 60 * 31))
+        snprintf(out,256,"%d weeks ago",(int)floor(deltaMinutes/(60 * 24 * 7)));
+    else if (deltaMinutes < (24 * 60 * 365.25))
+        snprintf(out,256,"%d months ago",(int)floor(deltaMinutes/(60 * 24 * 30)));
+    else
+        snprintf(out,256,"%d years ago",(int)floor(deltaMinutes/(60 * 24 * 365)));
+
+    return 0;
+}
+
--- src/webui/src/webui.c
+++ src/webui/src/webui.c
@@ -21,57 +21,7 @@
 #define __WEBUI_MAIN_FILE__
 #include <webui.h>
 
-int relative_time(char *utc, char *out) {
-    time_t nowutc;
-    time_t now;
-    time_t then;
-    long int deltaSeconds;
-    long int deltaMinutes;
-
-    struct tm tt;
-    // parse utc into a tm struct
-    memset(&tt,0,sizeof(struct tm));
-    if( ! strptime(utc, "%Y-%m-%dT%H:%M:%SZ", &tt) ) {
-        // kore_log(LOG_ERR,"relative_time failed parsing UTC string: %s",utc);
-        if( ! strptime(utc, "%Y-%m-%dT%H:%M:%S", &tt) ) {
-         	sprintf(out,"n/a");
-
-            return 1;
-
-        }
-    	}
-    then = mktime(&tt);
-    
-    // gets what UTC time is now
-    time( &nowutc );
-    now = mktime( gmtime ( &nowutc ) );
-
-    // localtime_r( &rnow, &now );
-    deltaSeconds = now - then;
-    deltaMinutes = (now / 60) - (then / 60);
-
-    if(deltaSeconds < 60)
-        snprintf(out,256,"%ld seconds ago", deltaSeconds);
-    else if (deltaMinutes < 60)
-        snprintf(out,256,"%ld minutes ago", deltaMinutes);
-    else if (deltaMinutes < (24 * 60))
-        snprintf(out,256,"%d hours ago",(int)floor(deltaMinutes/60));
-    else if (deltaMinutes < (24 * 60 * 7))
-        snprintf(out,256,"%d days ago",(int)floor(deltaMinutes/(60 * 24)));
-    else if (deltaMinutes < (24 * 60 * 31))
-        snprintf(out,256,"%d weeks ago",(int)floor(deltaMinutes/(60 * 24 * 7)));
-    else if (deltaMinutes < (24 * 60 * 365.25))
-        snprintf(out,256,"%d months ago",(int)floor(deltaMinutes/(60 * 24 * 30)));
-    else
-        snprintf(out,256,"%d years ago",(int)floor(deltaMinutes/(60 * 24 * 365)));
-
-    return 0;
-}
-
-
-
-int thing_show_cb( attributes_set_t  *data, int argc, MYSQL_ROW argv, MYSQL_FIELD *azColName)
-{
+int thing_show_cb( attributes_set_t  *data, int argc, MYSQL_ROW argv, MYSQL_FIELD *azColName) {
 #define SIZE (256)
     int i;
 
