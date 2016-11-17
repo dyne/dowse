b65c3d86630b724ecf933459a1859a9c69fd85a3
Nicola <nicola@dyne.org>
2016-11-17 14:14:26 +0100

    Fixed validation from source IP address

--- src/webui/src/get_ip_from_request.c
+++ src/webui/src/get_ip_from_request.c
@@ -8,10 +8,37 @@
 #include <webui.h>
 
 int get_ip_from_request(struct http_request *req,
-        char* ipaddr_type,int sizeof_ipaddr_type,
-        char *ipaddr_value,int sizeof_ipaddr_value
+        char ** ipaddr_type,
+        char **ipaddr_value
         ) {
 
+#ifndef PROC_FILE_FOR_NETWORK
+//    req->owner->addr->ipv4.sin_addr ;
+    char _ip[64];
+    memset(_ip,0,sizeof(_ip));
+    char *s;
+#define S 6
+    (*ipaddr_type)=kore_malloc(S);
+
+    if (req->owner->addrtype==AF_INET) {
+        s=inet_ntop(req->owner->addrtype,
+                &(req->owner->addr.ipv4.sin_addr),
+                _ip,sizeof(_ip)
+        );
+        snprintf(*ipaddr_type,S,"ipv4");
+    } else {
+        s=inet_ntop(req->owner->addrtype,
+                &(req->owner->addr.ipv6.sin6_addr),
+                _ip,sizeof(_ip)
+        );
+        snprintf(*ipaddr_type,S,"ipv6");
+    }
+    (*ipaddr_value)=kore_malloc(strlen(_ip));
+    sprintf( *ipaddr_value,"%s",_ip);
+
+    kore_log(LOG_DEBUG,"%s connection from %s",__where_i_am__,s);
+
+#else
     char socket_link_path[256];
     sprintf(socket_link_path,"/proc/%d/fd/%d",getpid(),req->owner->fd);
 
@@ -26,7 +53,7 @@ int get_ip_from_request(struct http_request *req,
 
     kore_log(LOG_DEBUG,"%s Hello your IP is [%s] and your address is [%s]",
             __where_i_am__,ipaddr_type,ipaddr_value);
-
+#endif
 
 
     return (KORE_RESULT_OK);
