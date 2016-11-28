bf97a241c204e9fdd2c1ae2eb818d9b3de23e13e
Nicola <nicola@dyne.org>
2016-11-28 14:42:06 +0100

    Added first version of "ip neigh" output parser

---
+++ src/parse-ip-neigh.c
@@ -0,0 +1,129 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char line[1024];
+char*field;
+char *fields[128];
+char ip4[128],ip6[256];
+char device[128];
+char macaddr[128];
+char ref_value[128];
+char used_str[128];
+char probes_str[128];
+
+#define DELIM " \n"
+int n_fields,i;
+
+int is_deleted,is_missing,is_router,is_proxy;
+
+int reset_fields_value(){
+  is_deleted=0;
+  is_missing=0;
+}
+
+void parse_constant(char**fields,char*value,int*ptr_idx,int*out_value){
+  if (strcmp(fields[*ptr_idx],value)==0) {
+    *out_value=1;
+    (*ptr_idx)++;
+  } else {
+    out_value=0;
+  }
+}
+
+void parse_ip4(char**fields,int*ptr_idx,char*value){
+  struct in_addr result;
+  int a,b,c,d;
+  
+  if (inet_pton(AF_INET,fields[*ptr_idx],&result)==1) {
+    sprintf(value,"%s",fields[*ptr_idx]);
+    (*ptr_idx)++;
+  } else {
+    sprintf(value,"");
+  }
+}
+
+
+void parse_ip6(char**fields,int*ptr_idx,char*value){
+  struct in6_addr result;
+
+  if (inet_pton(AF_INET6,fields[*ptr_idx],&result)==1) {
+    sprintf(value,"%s",fields[*ptr_idx]);
+    (*ptr_idx)++;
+  } else {
+    sprintf(value,"");
+  }
+}
+
+void parse_parameter(char**fields,int*ptr_idx,char*parameter,char*out_value){
+  if (strcmp(fields[*ptr_idx],parameter)==0) {
+    sprintf(out_value,"%s",fields[1+*ptr_idx]);
+    (*ptr_idx)+=2;
+  } else {
+    sprintf(out_value,"");
+  }
+}
+
+#define parse_state(S) \
+int IS_##S ;\
+IS_##S = (strstr(fields[n_fields-1], #S )!=0);\
+
+
+
+int main(){
+
+  for (;!feof(stdin);fgets(line,sizeof(line),stdin)) {
+    #ifdef _DEBUG
+    fprintf(stderr,"\n--------\nNew line\n");
+    #endif
+    field=strtok(line,DELIM);
+    for (n_fields=0;field;n_fields++) {
+      fields[n_fields]=(char*)malloc(strlen(field)+1);
+      sprintf(fields[n_fields],"%s",field);
+      field=strtok(NULL,DELIM);
+    }
+    fields[n_fields]=field;
+
+    reset_fields_value();
+    #ifdef _DEBUG
+    for (i=0;i<n_fields;i++) {
+      fprintf(stderr,"%d\t[%s]\n",i,fields[i]);
+    }
+    #endif
+    if (n_fields<=0) continue;
+    i=0;
+    parse_constant(fields,"Deleted",&i,&is_deleted);
+    parse_constant(fields,"miss",&i,&is_missing);
+
+    parse_ip4(fields,&i,ip4);
+    parse_ip6(fields,&i,ip6);
+    
+
+    parse_parameter(fields,&i,"dev",device);
+    parse_parameter(fields,&i,"lladdr",macaddr);
+
+    parse_constant(fields,"router",&i,&is_router);
+    parse_constant(fields,"proxy",&i,&is_proxy);
+    
+    parse_parameter(fields,&i,"ref",ref_value);
+    parse_parameter(fields,&i,"used",used_str);
+    parse_parameter(fields,&i,"probes",probes_str);
+
+    /* */
+    parse_state(INCOMPLETE);
+    parse_state(REACHABLE);
+    parse_state(STALE);
+    parse_state(DELAY);
+    parse_state(PROBE);
+    parse_state(FAILED);
+    parse_state(NOARP);
+    parse_state(PERMANENT);
+
+    /* TODO Generate the SQL code */
+    fprintf(stderr,"MAC [%s]\tIP4 [%s]\tIP6 [%s]\tState:[%s] [%s]\n",macaddr,ip4,ip6,(IS_REACHABLE?"reachable":""),(IS_FAILED?"failed":""));
+
+  }
+
+}
+		    
