a6c40faf992a241311dc66577d3244a33f9171c5
Jaromil <jaromil@dyne.org>
2016-12-18 13:35:42 +0100

    included dnscrypt-proxy as submodule

--- src/dnscrypt-proxy/dist-build/osx.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-export CFLAGS="-mmacosx-version-min=10.8 -march=core2 -O2 -g"
-export LDFLAGS="-mmacosx-version-min=10.8 -march=core2 -O2 -g"
-
-./configure --with-included-ltdl \
-            --enable-plugins-root && \
-make -j3
