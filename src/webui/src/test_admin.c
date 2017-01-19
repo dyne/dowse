7de1bb3566e876947dba6001bd29e79ad99c649c
Nicola <nicola@dyne.org>
2017-01-19 11:53:50 +0100

    Cleaning

--- src/webui/src/test_admin.c
+++ src/webui/src/test_admin.c
@@ -8,10 +8,10 @@
 #include <webui.h>
 
 int test_admin(struct http_request * req) {
+    log_entering();
     char message[256];
     int len;
 
-    WEBUI_DEBUG;
     sprintf(message,"Hello this is the Admin's page");
     http_response(req, 200, message, strlen(message));
 
