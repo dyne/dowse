35274ebdbe3540cee2e42609fe8817b8b75eedfc
Nicola <nicola@dyne.org>
2017-03-15 22:00:04 +0100

    Added ALL_THINGS_OFF command

--- src/webui/src/queue_command.c
+++ src/webui/src/queue_command.c
@@ -56,18 +56,34 @@ int queue_command(struct http_request * req) {
     redis_context=redis;
 
 
-    if (strcmp(op,"THING_ON")==0) {
-        int rv=change_authorization_to_browse(req,macaddr,"","",redis,1);
+    if ((strcmp(op,"THING_ON")==0)||(strcmp(op,"THING_OFF")==0)) {
+        int rv=change_authorization_to_browse(req,macaddr,"","",redis,op);
         if (rv!=KORE_RESULT_OK) {
             return rv;
         }
     }
-    if (strcmp(op,"THING_OFF")==0) {
-        int rv=change_authorization_to_browse(req,macaddr,"","",redis,0);
+    if (strcmp(op,"ALL_THINGS_OFF")==0) {
+        char *ipaddr_type,*calling_ipaddr;
+        char ip4[64],ip6[256];
+        char calling_macaddr[32];
+
+        get_ip_from_request(req,&ipaddr_type,&calling_ipaddr);
+        ip4[0]=ip6[0]=0;
+        if (strcmp(ipaddr_type,"ipv4")==0) {
+            sprintf(ip4,"%s",calling_ipaddr);
+        }
+        if (strcmp(ipaddr_type,"ipv6")==0) {
+            sprintf(ip6,"%s",calling_ipaddr);
+        }
+
+        ip2mac(ipaddr_type,calling_ipaddr,calling_macaddr,&attr);
+
+        int rv=change_authorization_to_browse(req,macaddr,ip4,ip6,redis,op);
         if (rv!=KORE_RESULT_OK) {
             return rv;
         }
     }
+
 
     /* Setup the URI for redirection , if it's present referer otherwise we redirect to /things */
 
