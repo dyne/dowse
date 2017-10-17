165e7c13522601f58bd5950ebe78a0fd846a254a
parazyd <parazyd@dyne.org>
2017-10-17 20:37:12 +0200

    remove maria2redis

--- src/maria2redis/test/install_json_udf.sql
+++
@@ -1,8 +0,0 @@
-DROP FUNCTION IF EXISTS json_members;
-DROP FUNCTION IF EXISTS json_array;
-DROP FUNCTION IF EXISTS json_object;
-DROP FUNCTION IF EXISTS json_values;
-CREATE FUNCTION json_members RETURNS STRING SONAME 'lib_mysqludf_json.so';
-CREATE FUNCTION json_array RETURNS STRING SONAME 'lib_mysqludf_json.so';
-CREATE FUNCTION json_object RETURNS STRING SONAME 'lib_mysqludf_json.so';
-CREATE FUNCTION json_values RETURNS STRING SONAME 'lib_mysqludf_json.so';
--- src/maria2redis/test/test_redis_udf.sql
+++
@@ -1,5 +0,0 @@
-select redis_command_v2("127.0.0.1",6379,"lpush","crmInboxEvents11",json_object(json_members("op","insert","value","valuettt")));
-select redis_servers_set_v2("127.0.0.1",6379);
-select redis_command_v2("127.0.0.1",6379,"lpush","crmInboxEvents11",json_object(json_members("op","insert","value","valuettt")));
-select free_resources();
-select redis_servers_set_v2("127.0.0.1",6379);
