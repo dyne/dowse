6315d88f614a2944c5c3cf3cb8fd0a1c6ba2d385
parazyd <parazyd@dyne.org>
2017-10-19 18:14:18 +0200

    fix some logic in parse-ip-neigh.py

--- src/parse-ip-neigh.py
+++ src/parse-ip-neigh.py
@@ -11,29 +11,33 @@ table = sys.stdin.readlines()
 thing_defaults = {'isadmin': 'no', 'enable_to_browse': 'no'}
 
 
-def fill_thing_info(thing):
+def fill_thing_info(thing, router=False):
     if RSTOR.exists('thing_%s' % thing[4]):
         RSTOR.hset('thing_%s' % thing[4], 'ip4', thing[0])
     else:
+        enable = 'no'
+        if router: enable = 'yes'
+
         thing_defaults = {
             'isadmin': 'no',
-            'enable_to_browse': 'no',
+            'enable_to_browse': enable,
             'macaddr': thing[4],
             'ip4': thing[0],
             'age': int(time()),
             'last': int(time()),
         }
-        RSTOR.hmset('thing_%s' % thing[4], thingdefaults)
+        RSTOR.hmset('thing_%s' % thing[4], thing_defaults)
 
 
 def main():
     for i in table:
         thing = i.split()
-        # we prefer that a thing is first reachable, otherwise we fallback to stale
-        if thing[5] == 'REACHABLE' and len(thing[4]) is 17:
-            fill_thing_info(thing)
-        elif thing[5] == 'STALE' and len(thing[4]) is 17:
-            fill_thing_info(thing)
+        if len(thing) is 6:
+            # we prefer that a thing is first reachable, otherwise we fallback to stale
+            if thing[5] == 'REACHABLE' and len(thing[4]) is 17:
+                fill_thing_info(thing)
+            elif thing[5] == 'STALE' and len(thing[4]) is 17:
+                fill_thing_info(thing)
 
 
 if __name__ == '__main__':
