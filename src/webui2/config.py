7f8846187073814d8801203c6e8cc065a5daf050
parazyd <parazyd@dyne.org>
2017-10-31 15:03:18 +0100

    webui: update config.py for new redis setup

--- src/webui2/config.py
+++ src/webui2/config.py
@@ -2,12 +2,13 @@ from redis import StrictRedis
 
 
 redis_host = "127.0.0.1"
-redis_port = 6379
+redis_storage_port = 6379
+redis_volatile_port = 6378
 
 redis_dynamic = 0
 redis_storage = 2
 
-RDYNA = StrictRedis(host=redis_host, port=redis_port, db=redis_dynamic,
-                    decode_responses=True)
-RSTOR = StrictRedis(host=redis_host, port=redis_port, db=redis_storage,
-                    decode_responses=True)
+RDYNA = StrictRedis(host=redis_host, port=redis_volatile_port,
+                    db=redis_dynamic, decode_responses=True)
+RSTOR = StrictRedis(host=redis_host, port=redis_storage_port,
+                    db=redis_storage, decode_responses=True)
