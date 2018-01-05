7e0c6c2f36b6820fb0a39396e5b0335e97ec19db
parazyd <parazyd@dyne.org>
2018-01-05 14:21:19 +0100

    update submodules after pulling master

--- ops/jenkins-build.sh
+++ ops/jenkins-build.sh
@@ -5,6 +5,7 @@ cd /home/dowse/dowse-src
 
 sudo -u dowse git clean -xdf && \
 sudo -u dowse git pull origin master && \
+sudo -u dowse git submodule update --init --checkout && \
 sudo -u dowse make && \
 mkdir -p destbuild && \
 make DESTDIR=/home/dowse/dowse-src/destbuild install || exit 1
