2df13d97bcf1b370d65bfa9fed41b298a08f5dcb
parazyd <parazyd@dyne.org>
2018-01-06 12:10:38 +0100

    Implement proper jenkins-build.sh

--- ops/jenkins-build.sh
+++ ops/jenkins-build.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 # This is executed as root when jenkins chroots
 
-set -x
+set -xe
 
-cd /home/dowse/dowse-src
+DOWSESOURCE="/home/dowse/dowse-src"
 
-git clean -xdf && \
-cd src/redis && git clean -xdf && cd - \
-sudo -u dowse git pull origin master && \
-sudo -u dowse git submodule update --init --checkout && \
-sudo -u dowse make && \
-mkdir -p destbuild && \
+cd "$DOWSESOURCE"
+git clean -xdf
+cd src/redis && git clean -xdf && cd -
+
+cat <<EOF > build-dowse.sh
+#!/bin/sh
+set -xe
+git pull origin master
+git submodule update --init --checkout
+make
+EOF
+
+sh build-dowse.sh || exit 1
+
+mkdir -p destbuild
 make DESTDIR=/home/dowse/dowse-src/destbuild install || exit 1
 
 cd destbuild
-tar czf /home/dowse/dowse-src/dowse-armhf-"$(date +%Y%m%d)".tar.gz . || exit 1
+tar czf "$DOWSESOUCE"/dowse-armhf-"$(date +%Y%m%d)".tar.gz . || exit 1
