e613cd98aba1fb9279e59e3ce1a8f9587e71af03
parazyd <parazyd@dyne.org>
2018-01-06 12:34:50 +0100

    Add comment about jenkins setup in jenkins-build.sh

--- ops/jenkins-build.sh
+++ ops/jenkins-build.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 # This is executed as root when jenkins chroots
 
+# Jenkins needs a prepared chroot for this to work. Our instance is amd64, so
+# we utilize qemu and binfmt-misc to enable chrooting into armhf userspace.
+# After the script finishes, it is then possible to archive the .tar.gz
+# artifact. Jenkins also needs to be able to use sudo without a password.
+#
+# The Jenkins shell script that uses this is the following:
+#
+#	CHROOTDIR="/var/lib/jenkins/chroots/dowse-ascii"
+#	DOWSESRC="/home/dowse/dowse-src"
+#
+#	sudo mount --rbind /dev "$CHROOTDIR"/dev
+#	sudo mount --rbind /sys "$CHROOTDIR"/sys
+#	sudo mount -t proc /proc "$CHROOTDIR"/proc
+#
+#	sudo chroot "$CHROOTDIR" "$DOWSESRC"/ops/jenkins-build.sh
+#
+#	sudo umount -R "$CHROOTDIR"/sys "$CHROOTDIR"/proc "$CHROOTDIR"/dev
+#	sudo mv -v "$CHROOTDIR/$DOWSESRC"/destbuild/*.tar.gz "$WORKSPACE" || exit 1
+#
+
 set -xe
 
 DOWSESOURCE="/home/dowse/dowse-src"
